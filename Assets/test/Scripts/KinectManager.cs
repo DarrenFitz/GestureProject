d7d1207de6932c8e417c48a075c5b5e7f52a1dbd
Darren Fitzpatrick <dfitzpatrick12@gmail.com>
2018-04-03 23:34:12 +0100

    project

---
+++ Assets/test/Scripts/KinectManager.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using Windows.Kinect;
+
+using System.Linq;
+
+public class KinectManager : MonoBehaviour
+{
+    private KinectSensor _sensor;
+    private BodyFrameReader _bodyFrameReader;
+    private Body[] _bodies = null;
+
+    public GameObject kinectAvailableText;
+    public Text handXText;
+
+    public bool IsAvailable;
+    public float ShipPosition;
+    public bool IsFire;
+    public bool IsPlaying;
+
+    public static KinectManager instance = null;
+
+    public Body[] GetBodies()
+    {
+        return _bodies;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+        Application.targetFrameRate = 60;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        _sensor = KinectSensor.GetDefault();
+
+        if (_sensor != null)
+        {
+            IsAvailable = _sensor.IsAvailable;
+
+            kinectAvailableText.SetActive(IsAvailable);
+
+            _bodyFrameReader = _sensor.BodyFrameSource.OpenReader();
+
+            if (!_sensor.IsOpen)
+            {
+                _sensor.Open();
+            }
+
+            _bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsAvailable = _sensor.IsAvailable;
+
+        if (_bodyFrameReader != null)
+        {
+            var frame = _bodyFrameReader.AcquireLatestFrame();
+
+            if (frame != null)
+            {
+                frame.GetAndRefreshBodyData(_bodies);
+
+                foreach (var body in _bodies.Where(b => b.IsTracked))
+                {
+                    IsAvailable = true;
+                    
+                    if (body.HandRightConfidence == TrackingConfidence.High && body.HandRightState == HandState.Lasso)
+                    {
+                        IsFire = true;
+                    }
+                    else
+                    {
+                        IsFire = false;
+                    }
+                    
+                    ShipPosition =  RescalingToRangesB(-1, 1, -3, 3, body.Lean.X);
+                    handXText.text = body.Lean.X.ToString();
+                    IsPlaying = true;
+                }
+                if (_bodies == null)
+                {
+                    Debug.Log("oh no");
+                    IsPlaying = false;
+                }
+                    
+
+                frame.Dispose();
+                frame = null;
+            }
+        }
+    }
+
+    static float RescalingToRangesB(float scaleAStart, float scaleAEnd, float scaleBStart, float scaleBEnd, float valueA)
+    {
+        return (((valueA - scaleAStart) * (scaleBEnd - scaleBStart)) / (scaleAEnd - scaleAStart)) + scaleBStart;
+    }
+
+    void OnApplicationQuit()
+    {
+        if (_bodyFrameReader != null)
+        {
+            _bodyFrameReader.IsPaused = true;
+            _bodyFrameReader.Dispose();
+            _bodyFrameReader = null;
+        }
+
+        if (_sensor != null)
+        {
+            if (_sensor.IsOpen)
+            {
+                _sensor.Close();
+            }
+
+            _sensor = null;
+        }
+    }
+}
+
+
+
+
+
---
+++ Assets/test/Scripts/shipcontrol.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipcontrol : MonoBehaviour {
+    public GameObject Bullet;
+    public float Speed = 1f;
+    private float time = 0;
+    private float cdTime = 0.2f;
+
+    private Vector3 playerPos = new Vector3(0, -3f, 0);
+
+    // Update is called once per frame
+    void Update () {
+
+        time += Time.deltaTime;
+        float xPos = transform.position.x;
+
+        if (KinectManager.instance.IsAvailable)
+        {
+            xPos = KinectManager.instance.ShipPosition;
+            if (KinectManager.instance.IsFire && time > cdTime)
+            {
+                Vector3 pos = gameObject.transform.position + new Vector3(0, 0.6f, 0);
+                Instantiate(Bullet, pos, gameObject.transform.rotation);
+                time = 0;
+            }
+        }
+        else
+        {
+            xPos = transform.position.x + (Input.GetAxis("Horizontal") * Speed);
+        }
+
+        playerPos = new Vector3(Mathf.Clamp(xPos, -3f, 3f), -3f, 0f);
+
+        transform.position = playerPos;
+    }
+}
+/*if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += new Vector3(-0.1f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 pos = gameObject.transform.position + new Vector3(0, 0.6f, 0);
+
+            Instantiate(Bullet, pos, gameObject.transform.rotation);
+
+        }*/
